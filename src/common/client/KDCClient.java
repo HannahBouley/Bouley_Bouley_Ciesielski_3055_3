53d0076a5f5f6bf851941f1ee970f75f640ccc5f
BrandonTB22 <bouleyb@merrimack.edu>
2025-03-17 22:24:37 -0400

    Command line support for kdcd.jar

--- src/common/client/KDCClient.java
+++ src/common/client/KDCClient.java
@@ -2,4 +2,14 @@ package client;
 
 public class KDCClient {
     
+    public static void main(String[] args) {
+        
+    }
+
+    /**
+     * Handles command line inputs
+     */
+    private static void handleCommandLineInput(){
+
+    }
 }
--- src/common/kdcd/KDCServer.java
+++ src/common/kdcd/KDCServer.java
@@ -4,6 +4,11 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import merrimackutil.cli.LongOption;
+import merrimackutil.cli.OptionParser;
+import merrimackutil.util.Tuple;
+
 import java.io.*;
 import java.net.*;
 import java.util.concurrent.*;
@@ -15,18 +20,23 @@ import java.util.concurrent.*;
 public class KDCServer {
 
     private static final int POOL_SIZE = 10; // MAX NUM OF CONNECTIONS
-    private static final int PORT_NUMBER = 5000; // PORT NUMBER THE SERVER IS RUNS ON
+    private static final int PORT_NUMBER = 5001; // PORT NUMBER THE SERVER IS RUNS ON
 
     // Entry point for the server
     public static void main(String[] args) {
 
+        handleCommandLineInput(args);
+
+        // Create a new thread pool to handle server-client connections
         ExecutorService executor = Executors.newFixedThreadPool(POOL_SIZE);
 
+        // Create a server socket with a port number
         try(ServerSocket serverSocket = new ServerSocket(PORT_NUMBER)){
-            System.out.println("Running on port " + PORT_NUMBER);
 
+            // Always keep the server running to accept incoming request from clients
             while (true) {
                 Socket socket = serverSocket.accept(); // Accept a new client to the server
+
                 executor.submit(new HandleClientConnections(socket));
                             }
                         } catch (IOException e){
@@ -35,7 +45,62 @@ public class KDCServer {
                             executor.shutdown();
                         }
                     }
-                }
+
+
+    /**
+     * Handles inputs from the command line
+     */
+    private static void handleCommandLineInput(String[] args){
+        
+        OptionParser optParser = new OptionParser(args);
+        Tuple<Character, String> currOpt;
+
+        // Set up the option parser
+        optParser.setOptString("hc:"); // Only need two commands
+
+        LongOption[] longOpts = new LongOption[2];
+
+        longOpts[0] = new LongOption("help", false, 'h'); // Help command
+        longOpts[1] = new LongOption("config", true, 'c'); // Congigure a file
+
+        optParser.setLongOpts(longOpts);
+
+      
+        while(optParser.getOptIdx() != args.length){
+            currOpt = optParser.getLongOpt(false);
+
+            switch (currOpt.getFirst()) {
+                case 'h': // Simply display the help menu
+                    System.out.println("usage:");
+                    System.out.println("    kdcd");
+                    System.out.println("    kdcd --config <configfile>");
+                    System.out.println("    kdcd --help");
+                    System.out.println("options:");
+                    System.out.println("    -c, --config Set the config file.");
+                    System.out.println("    -h, --help Display the help.");
+
+                    break;
+
+                case 'c': // Configure a file
+
+                    File configFile = new File(currOpt.getSecond());
+
+                    break;
+
+
+                case '?': // Finished with commands
+                    System.exit(1);
+                    break;
+            
+                default:
+                    System.exit(1);
+                    break;
+            }
+        }
+        
+    }
+}
+
                 
 /**
  * Handles client connections to the server
