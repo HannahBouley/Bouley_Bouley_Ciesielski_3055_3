265bcd514b994f7fb5722459647af8855257e1aa
BrandonTB22 <bouleyb@merrimack.edu>
2025-03-23 14:44:54 -0400

    Working on KDC Server protocol

--- src/common/client/KDCClient.java
+++ src/common/client/KDCClient.java
@@ -1,15 +1,21 @@
 package client;
 
+import java.io.Console;
+import java.util.logging.ConsoleHandler;
+
+/// import org.bouncycastle.jcajce.provider.keystore.bcfks.BcFKSKeyStoreSpi.StdShared;
+
+import merrimackutil.cli.LongOption;
+import merrimackutil.cli.OptionParser;
+import merrimackutil.util.Tuple;
+
 public class KDCClient {
+
+
     
     public static void main(String[] args) {
         
+       
     }
 
-    /**
-     * Handles command line inputs
-     */
-    private static void handleCommandLineInput(){
-
-    }
 }
--- src/common/kdcd/KDCServer.java
+++ src/common/kdcd/KDCServer.java
@@ -10,8 +10,10 @@ import merrimackutil.cli.OptionParser;
 import merrimackutil.json.JSONSerializable;
 import merrimackutil.json.JsonIO;
 import merrimackutil.json.parser.JSONParser;
+import merrimackutil.json.types.JSONArray;
 import merrimackutil.json.types.JSONObject;
 import merrimackutil.json.types.JSONType;
+import merrimackutil.util.NonceCache;
 import merrimackutil.util.Tuple;
 
 import java.io.*;
@@ -23,23 +25,34 @@ import java.util.concurrent.*;
 /**
  * The server in which the client will connect to
  */
-public class KDCServer {
-    // For reference: ./test-data/kdc-config/config.json
+public class KDCServer{
+    /**
+     *  For reference: 
+     * 
+     *  Config and run: java -jar dist/kdcd.jar -c ./test-data/kdc-config/config.json
+     *  Display help: java -jar dist/kdcd.jar -h
+     * */ 
 
     private static final int POOL_SIZE = 10; // MAX NUM OF CONNECTIONS
-    private static int PORT_NUMBER = 0; // PORT NUMBER THE SERVER IS RUNS ON
-    private static String secretsFile = null;
-    private static String validityPeriod = null;
+    private static final int nonceSize = 32;
+    
+    private static int PORT_NUMBER = 5000; // PORT NUMBER THE SERVER IS RUNS ON
+    private static String secretsFile = "./test-data/kdc-config/secrets.json"; // FILE THAT HOLDS SECRETS
+    private static String validityPeriod = "60000"; // THE PERIOD THAT THE TICKET IS VALID FOR
+    public static NonceCache nonceCache;
 
     // Entry point for the server
     public static void main(String[] args) {
 
+        // Create a new Nonce
+        nonceCache = new NonceCache(nonceSize, 60000);
+
+        // Read the commands on the command line
         handleCommandLineInput(args);
 
         // Create a new thread pool to handle server-client connections
         ExecutorService executor = Executors.newFixedThreadPool(POOL_SIZE);
 
-        System.out.println(PORT_NUMBER);
         // Create a server socket with a port number
         try(ServerSocket serverSocket = new ServerSocket(PORT_NUMBER)){
             
@@ -50,7 +63,6 @@ public class KDCServer {
                 Socket socket = serverSocket.accept(); // Accept a new client to the server
 
                 executor.submit(new HandleClientConnections(socket));
-
 
                             }
                         } catch (IOException e){
@@ -79,7 +91,6 @@ public class KDCServer {
 
         optParser.setLongOpts(longOpts);
 
-      
         while(optParser.getOptIdx() != args.length){
             currOpt = optParser.getLongOpt(false);
 
@@ -97,7 +108,7 @@ public class KDCServer {
                     System.exit(1);
                     break;
 
-                case 'c': // Configure a file
+                case 'c': // Configure w/ file, start the server
                     System.out.println("Configuring file...");
 
                     try {
@@ -110,7 +121,6 @@ public class KDCServer {
                         e.printStackTrace();
                     }
 
-                    
                     break;
 
 
@@ -137,35 +147,78 @@ public class KDCServer {
             tmp = (JSONObject) obj;
 
             if (tmp.containsKey("secrets-file")){
-                
                 secretsFile = tmp.getString("secrets-file");
             } else {
-                throw new InvalidObjectException("Expected field: secrets-file");
-                
+                throw new InvalidObjectException("Expected field: secrets-file");   
             }
-
             if (tmp.containsKey("port")){
                 PORT_NUMBER = tmp.getInt("port");
             } else {
                 throw new InvalidObjectException("Expected field: port");
-
             }
-
             if (tmp.containsKey("validity-period")){
                 validityPeriod = tmp.getString("validity-period");
             } else {
-                throw new InaccessibleObjectException("Expected field: validity-period");
+                throw new InvalidObjectException("Expected field: validity-period");
             }
+            System.out.println("Server configured!");
+        } 
+    }
 
-            System.out.println("File configured!");
+    private static void generateChallenge(String userName) throws Exception{
+
+        // Create a new nonce
+        byte[] nonce = nonceCache.getNonce();
+
+        // Determine if the username is in the secret store
+        JSONObject obj = JsonIO.readObject(new File("./test-data/kdc-config/" + secretsFile));
+
+        JSONObject tmp;
+        JSONArray tmpArry;
+
+        if (obj instanceof JSONObject){
+            tmp = (JSONObject) obj;
+            
+            if (tmp.containsKey("secrets")){
+                
+                if (tmp.getArray("secrets") instanceof JSONArray){
+
+                    tmpArry = (JSONArray) tmp.getArray("secrets");
+
+                    for (int i = 0; i < tmpArry.size(); i++){
+                        if(tmpArry.getObject(i).getString("user").equals(userName)){
+                            System.out.println("user found!");
+                        }
+                    }
+
+                    
+                } else {
+
+                    throw new InvalidObjectException("Expected type: JSONArray");
+                }
+               
+
+            } else {
+
+                throw new InvalidObjectException("Expected field: secrets");
+                
+            }
         }
-        
+
+
+
     }
 
 
+    public void sendChallengeToClient(){
+
+    }
+
+
+    
 }
 
-                
+
 /**
  * Handles client connections to the server
  */
@@ -180,7 +233,7 @@ class HandleClientConnections implements Runnable{
     @Override
     public void run() {
         
-    
+        
     }
 
 }
